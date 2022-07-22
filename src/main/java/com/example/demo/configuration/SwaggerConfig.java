b38cb8d1b310411533b3431135e0b029c3a509f1
neeju-bash <neerajmohan@gmail.com>
2022-07-22 10:28:07 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/example/demo/exception/ApplicationExceptionHandler.java
    #	src/main/java/com/example/demo/service/MovieService.java

--- src/main/java/com/example/demo/configuration/SwaggerConfig.java
+++ src/main/java/com/example/demo/configuration/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import com.google.common.base.Predicates;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,7 +20,7 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter
         //Also it is configuring the Swagger Docket
         return new Docket(DocumentationType.SWAGGER_2).select()
                 // .apis(RequestHandlerSelectors.any())
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("com.example.demo")))
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                 // .paths(PathSelectors.any())
                 // .paths(PathSelectors.ant("/swagger2-demo"))
                 .build();
